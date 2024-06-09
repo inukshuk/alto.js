fea6606e15625d2c951865d6939a4ad8ab7d2df5
Sylvester Keil <sylvester@keil.or.at>
2024-06-09 15:27:35 +0200

    Compute scale factor also when using pixels

--- test/document.test.js
+++ test/document.test.js
@@ -3,49 +3,53 @@ import { afterEach, before, describe, it } from 'node:test'
 import { Document } from 'alto.js'
 
 describe('Document', () => {
-  let inch
-  let pixel
+  let loc, tr
 
   before(() => {
-    inch = Document.parse(F('loc.xml'))
-    pixel = Document.parse(F('transkribus.xml'))
+    loc = Document.parse(F('loc.xml'))
+    tr = Document.parse(F('transkribus.xml'))
   })
 
   it('has a measurement unit', () => {
-    assert.equal(inch.unit, 'inch1200')
-    assert.equal(pixel.unit, 'pixel')
+    assert.equal(loc.unit, 'inch1200')
+    assert.equal(tr.unit, 'pixel')
   })
 
   describe('scale', () => {
     let init = { x: 1, y: 1 }
 
     afterEach(() => {
-      pixel.setScale()
-      inch.setScale()
+      loc.setScale()
+      tr.setScale()
     })
 
     it('has default scale without image dimensions', () => {
-      pixel.setScale()
-      assert.deepEqual(pixel.scale, init)
-      inch.setScale()
-      assert.deepEqual(pixel.scale, init)
+      tr.setScale()
+      assert.deepEqual(tr.scale, init)
+      loc.setScale()
+      assert.deepEqual(loc.scale, init)
     })
 
-    it('has default scale when using pixels', () => {
-      pixel.setScale(512, 512)
-      assert.deepEqual(pixel.scale, init)
-    })
-
-    it('has variable scale when using inch1200', () => {
-      let { page } = inch
+    it('has variable scale when using ', () => {
+      let { page } = loc
 
       let W = page.getAttribute('WIDTH')
       let H = page.getAttribute('HEIGHT')
       let w = 256
       let h = 512
       
-      inch.setScale(w, h)
-      assert.deepEqual(inch.scale, {
+      loc.setScale(w, h)
+      assert.deepEqual(loc.scale, {
+        x: w / W,
+        y: h / H
+      })
+
+      page = tr.page
+      W = page.getAttribute('WIDTH')
+      H = page.getAttribute('HEIGHT')
+
+      tr.setScale(w, h)
+      assert.deepEqual(tr.scale, {
         x: w / W,
         y: h / H
       })
