582a8499fdfb78ef2c0bb95a578c2d299aada720
Sylvester Keil <sylvester@keil.or.at>
2024-06-09 14:57:17 +0200

    Compute scale factor for inch1200 and mm10

--- test/document.test.js
+++ test/document.test.js
@@ -1,21 +1,54 @@
-import assert from 'node:assert'
-import { before, describe, it } from 'node:test'
+import assert from 'node:assert/strict'
+import { afterEach, before, describe, it } from 'node:test'
 import { Document } from 'alto.js'
 
 describe('Document', () => {
-  let alto
+  let inch
+  let pixel
 
   before(() => {
-    alto = Document.parse(F('doc.xml'))
+    inch = Document.parse(F('loc.xml'))
+    pixel = Document.parse(F('transkribus.xml'))
   })
 
   it('has a measurement unit', () => {
-    assert.equal(alto.unit, 'pixel')
+    assert.equal(inch.unit, 'inch1200')
+    assert.equal(pixel.unit, 'pixel')
   })
 
-  it('has a numerical resolution', () => {
-    let { resolution } = alto
-    assert(typeof resolution === 'number', 'not a number')
-    assert(!Number.isNaN(resolution), 'NaN')
+  describe('scale', () => {
+    let init = { x: 1, y: 1 }
+
+    afterEach(() => {
+      pixel.setScale()
+      inch.setScale()
+    })
+
+    it('has default scale without image dimensions', () => {
+      pixel.setScale()
+      assert.deepEqual(pixel.scale, init)
+      inch.setScale()
+      assert.deepEqual(pixel.scale, init)
+    })
+
+    it('has default scale when using pixels', () => {
+      pixel.setScale(512, 512)
+      assert.deepEqual(pixel.scale, init)
+    })
+
+    it('has variable scale when using inch1200', () => {
+      let { page } = inch
+
+      let W = page.getAttribute('WIDTH')
+      let H = page.getAttribute('HEIGHT')
+      let w = 256
+      let h = 512
+      
+      inch.setScale(w, h)
+      assert.deepEqual(inch.scale, {
+        x: w / W,
+        y: h / H
+      })
+    })
   })
 })
